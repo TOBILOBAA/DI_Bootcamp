5f1c917f70fc85ecc09feb70f41b40fe8f5eba88
tobilobaa <adeajayitoby@gmail.com>
2025-08-23 12:25:01 +0300

    My Excercises

--- Week_1/Day_2/daily_challenge/Daily_Challenge.py
+++ Week_1/Day_2/daily_challenge/Daily_Challenge.py
@@ -1,12 +1,12 @@
 # Challenge 1: Multiples of a Number
-# give_me_a_number = int(input("Give me a number: "))
-# length = int(input("How long do you want the list to be: "))
+give_me_a_number = int(input("Give me a number: "))
+length = int(input("How long do you want the list to be: "))
 
-# multiples = []
+multiples = []
 
-# for i in range(1, length + 1):
-#     multiples.append(give_me_a_number * i )
-#     print(multiples)
+for i in range(1, length + 1):
+    multiples.append(give_me_a_number * i )
+    print(multiples)
 
 # Challenge 2: Remove Consecutive Duplicate Letters
 
