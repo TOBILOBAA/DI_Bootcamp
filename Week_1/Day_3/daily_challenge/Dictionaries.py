a60bc343f436d78b07e21128fd5db1b2b0279b32
Tobi <68877853+TOBILOBAA@users.noreply.github.com>
2025-08-26 14:21:07 +0300

    My Daily Challenge

--- Week_1/Day_3/daily_challenge/Dictionaries.py
+++ Week_1/Day_3/daily_challenge/Dictionaries.py
@@ -1,16 +1,16 @@
-# # Challenge 1: Letter Index Dictionary
+# Challenge 1: Letter Index Dictionary
 
-# user_input = input("Enter a word: ")
-# my_dict = {}
+user_input = input("Enter a word: ")
+my_dict = {}
 
-# for index, char in enumerate(user_input):
-#     if char in my_dict:
-#         my_dict[char].append(index)
-#         print(f"Character {char} is a key in the dictionary")
-#     else: 
-#         my_dict[char] = [index]
-#         print(f"Character {char} is not a key in the dictionary")
-# print(my_dict)
+for index, char in enumerate(user_input):
+    if char in my_dict:
+        my_dict[char].append(index)
+        print(f"Character {char} is a key in the dictionary")
+    else: 
+        my_dict[char] = [index]
+        print(f"Character {char} is not a key in the dictionary")
+print(my_dict)
 
 
 # Challenge 2: Affordable Items
